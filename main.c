cd6d6114c2e8a1b582412c7ba747fcabff4bbf65
emz1003 <emzhang1003@gmail.com>
2019-11-14 00:01:36 -0500

    finish assignment

--- main.c
+++ main.c
@@ -1,1 +1,45 @@
-#include "stat.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main() {
+    struct stat s;
+    stat("makefile", &s);
+    int bytes = s.st_size;
+    printf("size in bytes: %lld\n", s.st_size);
+    printf("mode: %o\n", s.st_mode);
+    printf("time: %s\n", ctime(&s.st_mtime));
+
+    printf("size in kilobytes: %.5lf\n", bytes / (float) 0b10000000000);
+    printf("size in megabytes: %.5lf\n", bytes / (float) 0b11110100001001000000);
+    printf("size in gigabytes: %.5lf\n", bytes / (float) 0x40000000);
+
+    mode_t mode = s.st_mode - 0100000;
+    int p[3];
+    p[2] = mode % 010;
+    p[1] = mode % 0100 / 010;
+    p[0] = mode / 0100;
+    // printf("mode: %o %o %o\n", user, group, other);
+    printf("mode: ");
+    int i;
+    for(i = 0; i < 3; i++) {
+        if (p[i] & 0b100)
+            printf("r");
+        else
+            printf("-");
+
+        if (p[i] & 0b10)
+            printf("w");
+        else
+            printf("-");
+
+        if (p[i] & 0b1)
+            printf("x");
+        else
+            printf("-");
+    }
+    printf("\n");
+}
--- makefile
+++ makefile
@@ -1,6 +1,9 @@
-all: main.o stat.o
-	gcc -o program main.o stat.o
-main.o: main.c stat.h
-	gcc -c main.c stat.h
-stat.o: stat.c stat.h
-	gcc -c stat.c stat.h
+all: main.o
+	gcc -o program main.o
+main.o: main.c
+	gcc -c main.c
+run:
+	./program
+clean:
+	rm program *.o
+
